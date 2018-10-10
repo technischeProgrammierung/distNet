40e4e630dc297236b3e41fe45409baf34ca0b6f4
JonasJansenHS <jonas.jansen@hs-osnabrueck.de>
2018-10-10 19:55:22 +0200

    added essay; small changes to test.c

--- test.c
+++ test.c
@@ -3,31 +3,31 @@
 
 int main()
 {
-    Point_t netNodes[5];
+    Point_t netNodes[6];
     Point_t pointToBeChecked;
-    Net_t net  = *newNet();
+    Net_t net = *newNet();
 
-    netNodes[0] = *newPoint(0,0);
-    netNodes[1] = *newPoint(5,0);
-    netNodes[2] = *newPoint(5,5);
-    netNodes[3] = *newPoint(2.5,4);
-    netNodes[4] = *newPoint(0,5);
+    netNodes[0] = *newPoint(0, 0);
+    netNodes[1] = *newPoint(5, 0);
+    netNodes[2] = *newPoint(6, 2.5);
+    netNodes[3] = *newPoint(5, 5);
+    netNodes[4] = *newPoint(2.5, 4);
+    netNodes[5] = *newPoint(0, 5);
 
-    pointToBeChecked = *newPoint(2.5,4.2);
+    pointToBeChecked = *newPoint(2, 2.5);
 
-    for(int i = 0; i < 5; i++)
+    for (int i = 0; i < 6; i++)
     {
         appendPoint(&net, &netNodes[i]);
     }
 
     printNet(&net);
-    
-    if(isValidNet(&net))
+
+    if (isValidNet(&net))
     {
-        LineItem_t *ptr = net.lines->listStart;
         printf("The entered net is valid!\n");
 
-        if(isWithinNet(&net, &pointToBeChecked))
+        if (isWithinNet(&net, &pointToBeChecked))
         {
             printf("The point ");
             printPoint(&pointToBeChecked);
@@ -44,7 +44,6 @@ int main()
     {
         printf("No valid net \n");
     }
-    
 
     return 0;
 }
